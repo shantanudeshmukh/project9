2b0208c5d763e6e8121f32c5662dca4b657c59fa
Shantanu Deshmukh <39661017+shantanudeshmukh@users.noreply.github.com>
2018-10-15 17:48:03 -0400

    initial commit

---
+++ python/save_df_to_csv.py
@@ -0,0 +1,12 @@
+######################################################################
+Author: Shantanu V. Deshmukh 
+######################################################################
+import pandas as pd
+
+# save df to csv
+# possible seperators are 
+# \t tsv
+#, for csv
+# can delimit by specific character as well
+
+df_name.to_csv("filepath" + "filename.format", sep='\t')
